1a57f563095f50e56e9ef3d19e868ea905529c27
Carlos Lopez <carl.lopez@gmail.com>
2014-07-16 21:50:05 -0600

    Adding animated label programatically

--- First/ViewController.m
+++ First/ViewController.m
@@ -18,6 +18,38 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Create label and set frame
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(80, 190, 200, 50)];
+    label.alpha = 0.0f;
+    label.text = @"Getting somewhere.....";
+    
+    CATransition *transition = [CATransition animation];
+    transition.type = kCATransitionFade;
+	
+    // Finally add the label
+    [self.view addSubview:label];
+    
+    // Try to animate the label
+    [UIView beginAnimations:Nil context:Nil];
+    
+    [UIView setAnimationDuration:8];
+    
+    label.alpha = 1.0f;
+    
+    [UIView commitAnimations];
+}
+
+-(void)fadeIn
+{
+    [UIView beginAnimations:Nil context:Nil];
+
+    [UIView setAnimationDuration:2];
+
+    //label.transform=CGAffineTransformMakeTranslation(for X coordinate,for Y coordinate);
+    //label.text = @"new stuff";
+
+    [UIView commitAnimations];
 }
 
 - (void)didReceiveMemoryWarning
